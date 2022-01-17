9889134d1a21723fd82cdab768a82be3e3086d5c
stellalo <stellaylo267@gmail.com>
2022-01-17 08:40:57 -0800

    Initial Commit

---
+++ Museum2022/Museum2022App.swift
@@ -0,0 +1,17 @@
+//
+//  Museum2022App.swift
+//  Museum2022
+//
+//  Created by stella on 2022-01-17.
+//
+
+import SwiftUI
+
+@main
+struct Museum2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
